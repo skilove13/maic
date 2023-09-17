89ff3ee7d62cb7d964a1979215cccafac434d15f
skilove13 <skilove13@daum.net>
2023-09-17 15:56:34 +0900

    dataset_preprocessing2 update

--- smhong/dataset_preprocessing1.py
+++ smhong/dataset_preprocessing1.py
@@ -1,3 +1,9 @@
+"""
+1. ECG_child_numpy_train폴더의 파일들을 읽어서 리스트 형태로 저장
+2. 리스트 형태의 데이터를 12개로 나누고 하나의 npz파일 60000 -> (5000, 12) 형식으로 저장
+3. ecg_child_data.npz파일로 저장
+"""
+
 # library
 import json
 import os
@@ -8,6 +14,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import h5py
 import glob
+import time
 
 directory_path='E:/smhong/micai_ai_challange/dataset/ECG_child_numpy_train'
 
@@ -21,7 +28,7 @@ for i in range(len(ecg_child_data_path)):
     ecg_child_data.append(file_path)
 
 print(ecg_child_data_path[0:10])
-print(ecg_child_data[0:100])
+print(ecg_child_data[0:10])
 print("len(ecg_child_data) = ",len(ecg_child_data))
 
 ecg_data_example = np.load(ecg_child_data[0])
@@ -30,12 +37,15 @@ print("ecg_data_example[0]=", ecg_data_example)
 
 # 데이터를 저장할 리스트 초기화
 ecg_data_list = []
+i = 0
 # 각 파일을 읽어와서 리스트에 추가
 for file_path in ecg_child_data[0:]:
     ecg_data = np.load(file_path)
     # 데이터를 12개로 나누고 (5000, 12) 형식으로 저장
     ecg_data_split = np.array_split(ecg_data, 5000)
     ecg_data_list.append(ecg_data_split)
+    print(i)
+    i += 1
 
 # 리스트를 Numpy 배열로 변환
 ecg_child_data_array = np.array(ecg_data_list)
@@ -43,6 +53,10 @@ ecg_child_data_array = np.array(ecg_data_list)
 # ecg_data_array의 shape 확인
 print("ecg_data_array의 shape:", ecg_child_data_array.shape)
 
+start = time.time()  # 시작 시간 저장
 # npz 파일로 저장
 np.savez('ecg_child_data.npz', ecg_child_data_array=ecg_child_data_array)
+
+end = time.time()  # 종료 시간 저장
+print("npz save time :", end - start)  # 현재시각 - 시작시간 = 실행 시간
 
